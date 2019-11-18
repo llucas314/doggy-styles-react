017c417e91f783591ea19ffe56d045a5ab93364a
Ren Lucas <llucas314@gmail.com>
2019-11-18 15:28:41 -0500

    added header component folder and files

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Switch></Switch>
+    </div>
+  );
+}
+
+export default App;
