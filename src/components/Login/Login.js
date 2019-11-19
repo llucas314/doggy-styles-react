659022634bf741601e0f2084aaff9427c600cd0d
Ren Lucas <llucas314@gmail.com>
2019-11-18 21:45:38 -0500

    cleaned up code

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,23 +1,16 @@
 import React from "react";
 import Header from "../Header/Header";
 import "./Login.css";
-// import goodDog from "../../images/good_dog.png";
-// import badDog from "../../images/bad_dog.png";
+
 function Login() {
   return (
     <div className="login">
       <Header nav={true} />
       <main>
-        {/* <div className="img-wrap">
-          <img src={goodDog} alt="Good Dog" />
-        </div> */}
         <div className="text-wrap">
           <h1>Doggy Styles</h1>
           <h3>A friend for your dog, no matter their style.</h3>
         </div>
-        {/* <div className="img-wrap">
-          <img src={badDog} alt="Bad Dog" />
-        </div> */}
       </main>
     </div>
   );
