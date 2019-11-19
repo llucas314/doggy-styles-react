8f5f433d71bcaecf2e9d079690d81a589b1a2fb4
Ren Lucas <llucas314@gmail.com>
2019-11-18 20:16:50 -0500

    updated and styled breadcrumbs

--- src/components/Breadcrumbs/Breadcrumbs.js
+++ src/components/Breadcrumbs/Breadcrumbs.js
@@ -1,10 +1,18 @@
 import React from "react";
 import "./Breadcrumbs.css";
-
+import { Link } from "react-router-dom";
 function Breadcrumbs() {
   return (
     <div className="breadcrumbs">
-      <h3>Home</h3>
+      <div className="home-wrap">
+        <Link to="/" className="home-link">
+          <i class="fas fa-paw"></i>
+        </Link>
+      </div>
+      <Link to="/" className="home-link">
+        {" "}
+        > <span> Search</span>
+      </Link>
     </div>
   );
 }
