f39dd96e53a49581c9728adc5077c1eb6ca8bda0
Ren Lucas <llucas314@gmail.com>
2019-11-18 21:43:25 -0500

    updated login css

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../Header/Header";
-import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
-
+import "./Login.css";
+// import goodDog from "../../images/good_dog.png";
+// import badDog from "../../images/bad_dog.png";
 function Login() {
   return (
-    <div>
+    <div className="login">
       <Header nav={true} />
-      <Breadcrumbs />
+      <main>
+        {/* <div className="img-wrap">
+          <img src={goodDog} alt="Good Dog" />
+        </div> */}
+        <div className="text-wrap">
+          <h1>Doggy Styles</h1>
+          <h3>A friend for your dog, no matter their style.</h3>
+        </div>
+        {/* <div className="img-wrap">
+          <img src={badDog} alt="Bad Dog" />
+        </div> */}
+      </main>
     </div>
   );
 }
