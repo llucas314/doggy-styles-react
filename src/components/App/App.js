bc3ea752509ba3ca756ef5e71d079b512a72d152
Ren Lucas <llucas314@gmail.com>
2019-11-18 22:18:55 -0500

    added search component and styled

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import Login from "../Login/Login";
+import Search from "../Search/Search";
 
 function App() {
   return (
     <Switch>
-      <Route path="/" component={Login}></Route>
+      <Route path="/" exact component={Login}></Route>
+      <Route path="/search" exact component={Search}></Route>
+      <Route path="/search/results/:breed" component={Search}></Route>
     </Switch>
   );
 }
