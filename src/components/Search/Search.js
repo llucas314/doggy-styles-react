58a84652f1d2ef3efaddb50e82fe25b6e6a5e473
Ren Lucas <llucas314@gmail.com>
2019-11-19 10:58:27 -0500

    updated select box with fetch data

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../Header/Header";
 import { Link } from "react-router-dom";
+import axios from "axios";
 import "./Search.css";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
 
@@ -9,7 +10,9 @@ export default class Search extends Component {
     super(props);
 
     this.state = {
-      value: "German Shepard"
+      isLoaded: false,
+      breeds: [],
+      value: ""
     };
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
@@ -20,26 +23,47 @@ export default class Search extends Component {
   handleChange = e => {
     this.setState({ value: e.target.value });
   };
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:8080/breeds")
+      .then(res => this.setState({ breeds: res.data }))
+      .then(this.setState({ isLoaded: true }))
+      .catch(err => console.log(err));
+  }
   render() {
     return (
       <div className="search">
         <Header nav={true} />
         <Breadcrumbs />
-        <main>
-          <h1>Find your dog's breed</h1>
-          <form onSubmit={this.handleSubmit}>
-            <select value={this.state.value} onChange={this.handleChange}>
-              <option value="german shepard">German Shepard</option>
-              <option value="gsp">German Short-haired Pointer</option>
-              <option value="poodle">Poodle</option>
-              <option value="boxer">Boxer</option>
-              <option value="chihuahua">Chihuahua</option>
-            </select>
-            <Link to={`/search/results/${this.state.value}`}>
-              <i class="fas fa-dog"></i>
-            </Link>
-          </form>
-        </main>
+        {this.state.isLoaded ? (
+          <main>
+            <h1>Find your dog's breed</h1>
+            <form onSubmit={this.handleSubmit}>
+              <select value={this.state.value} onChange={this.handleChange}>
+                <option value="">Choose a breed</option>
+                {this.state.breeds.map(breed => (
+                  <option key={breed._id} value={breed.name}>
+                    {breed.name}
+                  </option>
+                ))}
+              </select>
+              {this.state.value === "" ? (
+                <Link to={`/search`}>
+                  <i class="fas fa-dog"></i>
+                </Link>
+              ) : (
+                <Link to={`/search/results/${this.state.value}`}>
+                  <i class="fas fa-dog"></i>
+                </Link>
+              )}
+            </form>
+          </main>
+        ) : (
+          <main>
+            <h1>Loading...</h1>
+          </main>
+        )}
       </div>
     );
   }
