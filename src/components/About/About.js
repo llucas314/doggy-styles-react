cb427b2391f7cbb42acf954b2a63a1a339fc8b57
Ren Lucas <llucas314@gmail.com>
2019-11-20 11:57:05 -0500

    updated password function

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,7 +9,7 @@ export default class About extends Component {
     super(props);
 
     this.state = {
-      user: [{}],
+      user: {},
       isLoaded: false,
       hidden: true,
       password: "",
@@ -34,15 +34,19 @@ export default class About extends Component {
 
   updatePassword = e => {
     e.preventDefault();
-    const url =
-      "https://cors-anywhere.herokuapp.com/localhost:8080/users/create";
+    const url = `https://doggystyle-api.herokuapp.com/users/update/${this.state.user._id}`;
     if (this.state.password === this.state.confirm) {
       axios
-        .post(url, {
+        .put(url, {
           password: this.state.password
         })
         .then(res => {
           console.log(res);
+        })
+        .then(() => {
+          const user = { ...this.state.user };
+          user.password = this.state.password;
+          this.setState({ user });
         })
         .then(this.setState({ pwChanged: true }))
         .catch(err => console.log(err));
@@ -63,7 +67,7 @@ export default class About extends Component {
     e.preventDefault();
     axios
       .delete(
-        `https://doggystyle-api.herokuapp.com/users/delete/${this.state.user[0]._id}`
+        `https://doggystyle-api.herokuapp.com/users/delete/${this.state.user._id}`
       )
       .then(res => {
         console.log(res);
@@ -78,86 +82,95 @@ export default class About extends Component {
         `https://doggystyle-api.herokuapp.com/users/${this.props.location.state.username}/${this.props.location.state.password}`
       )
       .then(res => {
-        this.setState({ user: res.data });
+        const userJson = { ...res.data[0] };
+        this.setState({ user: userJson });
         this.setState({ dogs: res.data[0].dogs });
+        this.setState({ isLoaded: true });
       })
-      .then(this.setState({ isLoaded: true }))
       .catch(err => console.log(err));
   }
 
   render() {
     if (this.state.deleted) {
       return <Redirect to="/" />;
+    } else if (this.state.isLoaded) {
+      return (
+        <div className="about">
+          <Header login={true} />
+          {this.state.isLoaded ? (
+            <main>
+              <div className="text-wrap">
+                <h1>Hello, {this.state.user.firstName}</h1>
+                <h2>Would you like to update your account?</h2>
+                <h4>Dogs</h4>
+                <ul className="dog-list">
+                  {this.state.dogs.map((dog, i) => (
+                    <li key={i} className="dog">
+                      <p>
+                        {dog.petName}
+                        <br />
+                        -Energy Level: {dog.energy_level}
+                        <br />
+                        -Age: {dog.age}
+                        {/* <br />
+                    -Breed :{name} */}
+                      </p>
+                    </li>
+                  ))}
+                </ul>
+                <h4>Add A Dog</h4>
+                <h4>
+                  {this.state.pwChanged
+                    ? "Password Updated"
+                    : `Change password ${this.state.message}`}
+                </h4>
+                <form onSubmit={this.updatePassword}>
+                  <label>
+                    Password:
+                    <input
+                      type={this.state.hidden ? "password" : "text"}
+                      name="password"
+                      onChange={this.handleChange}
+                    />
+                  </label>
+                  <label>
+                    Confirm Password:
+                    <input
+                      type={this.state.hidden ? "password" : "text"}
+                      name="confirm"
+                      onChange={this.handleChange}
+                    />
+                    <button onClick={this.toggleShow}>Show / Hide</button>
+                  </label>
+                  <input type="submit" value="Submit" />
+                </form>
+                <h4>Delete Account</h4>
+                <button onClick={this.modalDelete}>Delete</button>
+                {this.state.modal ? (
+                  <div className="delete-modal">
+                    <h3>Are You Sure?</h3>
+                    <button onClick={this.handleDelete}>Yes</button>
+                    <button onClick={() => this.setState({ modal: false })}>
+                      NO
+                    </button>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </div>
+            </main>
+          ) : (
+            ""
+          )}
+        </div>
+      );
     }
-
     return (
       <div className="about">
         <Header login={true} />
-        {this.state.isLoaded ? (
-          <main>
-            <div className="text-wrap">
-              <h1>Hello, {this.state.user[0].firstName}</h1>
-              <h2>Would you like to update your account?</h2>
-              <h4>Dogs</h4>
-              <ul className="dog-list">
-                {this.state.dogs.map((dog, i) => (
-                  <li key={i} className="dog">
-                    <p>
-                      {dog.petName}
-                      <br />
-                      -Energy Level: {dog.energy_level}
-                      <br />
-                      -Age: {dog.age}
-                      {/* <br />
-                      -Breed :{name} */}
-                    </p>
-                  </li>
-                ))}
-              </ul>
-              <h4>Add A Dog</h4>
-              <h4>
-                {this.state.pwChanged
-                  ? "Password Updated"
-                  : `Change password ${this.state.message}`}
-              </h4>
-              <form onSubmit={this.updatePassword}>
-                <label>
-                  Password:
-                  <input
-                    type={this.state.hidden ? "password" : "text"}
-                    name="password"
-                    onChange={this.handleChange}
-                  />
-                </label>
-                <label>
-                  Confirm Password:
-                  <input
-                    type={this.state.hidden ? "password" : "text"}
-                    name="confirm"
-                    onChange={this.handleChange}
-                  />
-                  <button onClick={this.toggleShow}>Show / Hide</button>
-                </label>
-                <input type="submit" value="Submit" />
-              </form>
-              <h4>Delete Account</h4>
-              <button onClick={this.modalDelete}>Delete</button>
-              {this.state.modal ? (
-                <div className="delete-modal">
-                  <h3>Are You Sure?</h3>
-                  <button onClick={this.handleDelete}>Yes</button>
-                  <button onClick={() => this.setState({ modal: false })}>
-                    NO
-                  </button>
-                </div>
-              ) : (
-                ""
-              )}
-            </div>
-          </main>
-        ) : (
-          ""
-        )}
+        <main>
+          <div className="text-wrap">Loading...</div>
+        </main>
       </div>
     );
   }
