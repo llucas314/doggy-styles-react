ca3fbdd86dce792e7ef2ba939ffc847214f5fcc3
Ren Lucas <llucas314@gmail.com>
2019-11-21 09:30:03 -0500

    search refactored

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -19,6 +19,7 @@ export default class Search extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
+
   handleSubmit = e => {
     e.preventDefault();
   };
@@ -27,48 +28,42 @@ export default class Search extends Component {
   };
 
   componentDidMount() {
-    axios
-      .get("https://doggystyle-api.herokuapp.com/breeds")
-      .then(res => this.setState({ breeds: res.data }))
-      .then(this.setState({ isLoaded: true }))
-      .catch(err => console.log(err));
+    // axios
+    //   .get("https://doggystyle-api.herokuapp.com/breeds")
+    //   .then(res => this.setState({ breeds: res.data }))
+    //   .then(this.setState({ isLoaded: true }))
+    //   .catch(err => console.log(err));
   }
   render() {
     return (
       <div className="search">
         <Header login={true} />
         <Breadcrumbs links={["Search"]} />
-        {this.state.isLoaded ? (
-          <main>
-            <h1>Find your dog's breed</h1>
-            <form onSubmit={this.handleSubmit}>
-              <Select
-                default={this.state.value}
-                handleChange={this.handleChange}
-                array={this.state.breeds}
-                selectOptions={"Choose a Breed"}
-              />
-              {this.state.value === "" ? (
-                <Link to={`/search`}>
-                  <Dog />
-                </Link>
-              ) : (
-                <Link
-                  to={{
-                    pathname: `/breeds/${this.state.value}`,
-                    state: { breeds: this.state.breeds }
-                  }}
-                >
-                  <Dog />
-                </Link>
-              )}
-            </form>
-          </main>
-        ) : (
-          <main>
-            <h1>Loading...</h1>
-          </main>
-        )}
+
+        <main>
+          <h1>Find your dog's breed</h1>
+          <form onSubmit={this.handleSubmit}>
+            <Select
+              default={this.state.value}
+              handleChange={this.handleChange}
+              array={this.props.breeds}
+              selectOptions={"Choose a Breed"}
+            />
+            {this.state.value === "" ? (
+              <Link to={`/search`}>
+                <Dog />
+              </Link>
+            ) : (
+              <Link
+                to={{
+                  pathname: `/breeds/${this.state.value}`
+                }}
+              >
+                <Dog />
+              </Link>
+            )}
+          </form>
+        </main>
       </div>
     );
   }
