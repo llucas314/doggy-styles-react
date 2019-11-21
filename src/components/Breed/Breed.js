9fd1f7c430cf82835d9d2ba4731936dcb1420540
Ren Lucas <llucas314@gmail.com>
2019-11-21 10:28:18 -0500

    working on img load

--- src/components/Breed/Breed.js
+++ src/components/Breed/Breed.js
@@ -11,14 +11,14 @@ export default class Breed extends Component {
     super(props);
 
     this.state = {
-      breeds: this.props.location.state.breeds,
+      breeds: [],
       dogBreed: "",
       isLoaded: false
     };
+    this.loadDog = this.loadDog.bind(this);
   }
-  componentDidMount() {
-    console.log("component did mount");
-    this.state.breeds.forEach(breed => {
+  loadDog = () => {
+    this.props.breeds.forEach(breed => {
       if (breed.name === this.props.match.params.id) {
         this.setState({ breeds: breed });
         axios
@@ -30,22 +30,48 @@ export default class Breed extends Component {
           .catch(err => console.log(err));
       }
     });
+  };
+  componentDidMount() {
+    this.loadDog();
+    // console.log(currentBreed);
+    // this.loadDog();
+    // this.props.breeds.forEach(breed => {
+    //   if (breed.name === this.props.match.params.id) {
+    //     this.setState({ breeds: breed });
+    //     axios
+    //       .get(
+    //         `https://api.thedogapi.com/v1/images/search?breed_ids=${breed.id}&api_key=22ea4027-cfc1-464e-89c4-aed63db671ad`
+    //       )
+    //       .then(res => this.setState({ dogBreed: res.data[0] }))
+    //       .then(this.setState({ isLoaded: true }))
+    //       .catch(err => console.log(err));
+    //   }
+    // });
   }
 
   render() {
+    const currentBreed = this.props.breeds.filter(
+      breed => breed.name === this.props.match.params.id
+    );
+
+    console.log(currentBreed);
+
     return (
       <div className="breed-page">
         <Header login={true} />
         <Breadcrumbs links={["Search", "Breeds"]} />
         <main>
-          <BreedProfile {...this.state.breeds} url={this.state.dogBreed.url} />
+          <BreedProfile
+            {...currentBreed[0]}
+            // url={this.state.dogBreed.url}
+          />
           <div className="link-wrap">
             <Link to="/search" className="breed-link">
               Back to Search
             </Link>{" "}
             |{" "}
             <Link
-              to={`/search/results/${this.state.breeds.name}`}
+              to={`/search/results/${currentBreed.name}`}
               className="breed-link"
             >
               {" "}
